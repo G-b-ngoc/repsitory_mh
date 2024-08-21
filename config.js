033e967e8aa8b31f7b69b4a929c153c313fdfcca
Giàng Bảo Ngọc <137142411+G-b-ngoc@users.noreply.github.com>
2024-08-21 23:01:25 +0700

    mh

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  titleWeb: "Gửi Đến Minh Hằng",
+  introTitle: "Măng",
+  introDesc: `Thế Giới Nay Nhỏ, Nhưng 
+     Ko Có Nghĩa Chúng Ta Sẽ Gặp Nhau Lần Nữa, Nhưng Tao Với Mày Thì Khác.`,
+  btnIntro: "^^Tiếp Tục^^",
+  title: "Xem lời nhắn",
+  desc: "chọn để xem",
+  btnYes: " Tôi là Chó ",
+  btnNo: " Bạn là chó ",
+  question: "Gửi Tới Admin!",
+  btnReply: "Lưu Lại",
+  reply:
+    "Đc Gặp m, T luôn cảm nhận được một sự chân thành mà không phải ai cũng có, và điều đó khiến T cảm thấy đây là một mối quan hệ rất đặc biệt.",
+  mess: "Ngày Tháng đó tuyệt.",
+  messDesc: "Muốn Xem Tiếp Không ꨄ︎ .",
+  btnAccept: "(Ờ)",
+  messLink: "https://photos.app.goo.gl/ZX4kxG5CZMomJ2zf8", //link mess của các bạn. VD: /dinhkaito
+};
